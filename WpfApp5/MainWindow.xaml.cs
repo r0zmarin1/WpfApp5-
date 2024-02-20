f0c1ccac8486bb25e5205afeb65d34b1ddaf1ed8
r0zmarin1 <irozina2006@gmail.com>
2024-02-21 01:21:00 +1000

    Добавьте файлы проекта.

---
+++ WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public class Tasks
+        {
+            public string Description { get; set; }
+            public string CreationDate { get; set; }
+            public string Emergency {  get; set; }
+            public string Status { get; set; }
+            public string CompletionDate { get; set; }
+        }
+
+        private void DoneButton(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CancelButton(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
